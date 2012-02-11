273cce30f08ba9d137a67a617cf0ba3506183d21
Enrique Viñé Lerma <evlerma@autentia.com>
2012-02-11 09:12:32 +0100

    Initial commit

---
+++ Javanecos.java
@@ -0,0 +1,154 @@
+package org.javahispano.javacup.tacticas.javanecos;
+
+import org.javahispano.javacup.model.*;
+import org.javahispano.javacup.model.util.Constants;
+import org.javahispano.javacup.model.util.Position;
+import org.javahispano.javacup.model.engine.GameSituations;
+import org.javahispano.javacup.model.command.*;
+import org.javahispano.javacup.tacticas.tacticas_aceptadas.arturo8a.TacticaPulgarcitos;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Javanecos implements Tactic {
+
+	// Lista de comandos
+    LinkedList<Command> comandos = new LinkedList<Command>();
+
+    // Alineaciones
+    private final Position alineacionBase[]=new Position[]{
+        new Position(0.0,-47.98642533936651),
+        new Position(17.11888111888112,-32.30769230769231),
+        new Position(0.2595419847328244,-31.082089552238806),
+        new Position(-18.307692307692307,-2.6131221719457014),
+        new Position(14.74125874125874,-3.088235294117647),
+        new Position(-16.643356643356643,21.61764705882353),
+        new Position(-17.11888111888112,-32.07013574660634),
+        new Position(14.503496503496503,22.092760180995477),
+        new Position(-26.867132867132867,32.07013574660634),
+        new Position(-0.23776223776223776,12.352941176470589),
+        new Position(24.965034965034967,31.357466063348415)
+    };
+
+    private final Position alineacionSaque[]=new Position[]{
+        new Position(0.23776223776223776,-48.223981900452486),
+        new Position(17.356643356643357,-32.54524886877828),
+        new Position(0.0,-23.993212669683256),
+        new Position(-14.265734265734267,-7.839366515837104),
+        new Position(12.839160839160838,-7.601809954751132),
+        new Position(-26.391608391608393,-1.4253393665158371),
+        new Position(-16.643356643356643,-31.119909502262445),
+        new Position(2.377622377622378,-1.4253393665158371),
+        new Position(17.832167832167833,-0.9502262443438915),
+        new Position(-5.944055944055944,-1.4253393665158371),
+        new Position(27.34265734265734,-1.4253393665158371)
+    };
+
+    private final Position alineacionDefensa[]=new Position[]{
+        new Position(0.23776223776223776,-48.223981900452486),
+        new Position(17.356643356643357,-32.54524886877828),
+        new Position(0.0,-23.993212669683256),
+        new Position(-18.545454545454543,-16.391402714932127),
+        new Position(7.608391608391608,-13.778280542986426),
+        new Position(-26.62937062937063,-1.900452488687783),
+        new Position(-16.643356643356643,-31.119909502262445),
+        new Position(10.6993006993007,-1.6628959276018098),
+        new Position(22.349650349650346,-16.628959276018097),
+        new Position(-13.79020979020979,-1.900452488687783),
+        new Position(25.202797202797203,-2.1380090497737556)
+    };
+
+    
+    
+    // Devuelve los jugadores a la posición inicial (sacando)
+    public Position[] getStartPositions(GameSituations sp) {
+    		return alineacionSaque;
+    }
+
+    // Devuelve los jugadores a la posición inicial (no sacando)
+    public Position[] getNoStartPositions(GameSituations sp) {
+        return alineacionDefensa;
+    }
+
+    // 
+    public TacticDetail getDetail() {
+        return new JavanecosTactilDetail();
+    }
+
+    @Override
+    // TODO este es el ejemplo de la web: modificar
+    public List<Command> execute(GameSituations sp) {
+        comandos.clear();
+        alineacionBase(sp);
+        
+        //Si no saca el rival
+//        if (!sp.isRivalStarts()) {
+            //Obtiene los datos de recuperacion del balon
+            int[] recuperadores = sp.getRecoveryBall();
+            //Si existe posibilidad de recuperar el balon
+            if (recuperadores.length > 1) {
+                //Obtiene las coordenadas del balon en el instante donde
+                //se puede recuperar el balon
+                double[] posRecuperacion = sp.getTrajectory(recuperadores[0]);
+                //Recorre la lista de jugadores que pueden recuperar
+                for (int i = 1; i < recuperadores.length; i++) {
+                     //Ordena a los jugadores recuperadores que se ubique
+                     //en la posicion de recuperacion
+                     comandos.add(new CommandMoveTo(recuperadores[i],
+                              new Position(posRecuperacion[0], posRecuperacion[1])));
+                }
+            }
+//        }
+        //Instancia un generador aleatorio
+        Random r = new Random();
+        //Recorre la lista de mis jugadores que pueden rematar
+        for (int i : sp.canKick()) {
+            //Si el jugador es de indice 8 o 10
+            if (i == 8 || i == 10) {
+                //condicion aleatoria
+                if (r.nextBoolean()) {
+                     //Ordena que debe rematar al centro del arco
+                     comandos.add(new CommandHitBall(i, Constants.centroArcoSup, 1, 12 + r.nextInt(6)));
+                } else if (r.nextBoolean()) {
+                     //Ordena que debe rematar al poste derecho
+                     comandos.add(new CommandHitBall(i, Constants.posteDerArcoSup, 1, 12 + r.nextInt(6)));
+                } else {
+                     //Ordena que debe rematar al poste izquierdo
+                     comandos.add(new CommandHitBall(i, Constants.posteIzqArcoSup, 1, 12 + r.nextInt(6)));
+                }
+            } else {
+                //inicia contador en cero
+                int count = 0;
+                int jugadorDestino;
+                //Repetir mientras el jugador destino sea igual al jugador que remata
+                while (((jugadorDestino = r.nextInt(11)) == i
+                         //o mientras la coordenada y del jugador que remata
+                         //es mayor que la coordenada y del que recibe
+                         || sp.myPlayers()[i].getY() > sp.myPlayers()[jugadorDestino].getY())
+                         //Y mientras el contador es menor a 20
+                         && count < 20) {
+                     //incrementa el contador
+                     count++;
+                }
+                //Si el receptor del balon es el que remata
+                if (i == jugadorDestino) {
+                     while ((jugadorDestino = r.nextInt(sp.myPlayers().length)) == i) {
+                     }
+                }
+                //Ordena que el jugador que puede rematar que de un pase
+                //al jugador destino
+                comandos.add(new CommandHitBall(i, sp.myPlayers()[jugadorDestino], 1, r.nextInt(45)));
+            }
+        }
+        //Retorna la lista de comandos
+        return comandos;
+    }
+
+    private void alineacionBase(GameSituations sp) {
+        for (int i = 0; i < sp.myPlayers().length; i++) {
+            //Ordena a cada jugador que se ubique segun la alineacion1
+            comandos.add(new CommandMoveTo(i, alineacionBase[i]));
+        }
+    }
+}
---
+++ JavanecosTactilDetail.java
@@ -0,0 +1,143 @@
+package org.javahispano.javacup.tacticas.javanecos;
+
+import java.awt.Color;
+
+import org.javahispano.javacup.model.PlayerDetail;
+import org.javahispano.javacup.model.TacticDetail;
+import org.javahispano.javacup.model.util.Position;
+import org.javahispano.javacup.render.EstiloUniforme;
+
+public class JavanecosTactilDetail implements TacticDetail {
+
+    public String getTacticName() {
+        return "Los Javañecos";
+    }
+
+    public String getCountry() {
+        return "España";
+    }
+
+    public String getCoach() {
+        return "Quique y Luis";
+    }
+
+    public Color getShirtColor() {
+        return new Color(37, 191, 11);
+    }
+
+    public Color getShortsColor() {
+        return new Color(0, 153, 153);
+    }
+
+    public Color getShirtLineColor() {
+        return new Color(17, 24, 207);
+    }
+
+    public Color getSocksColor() {
+        return new Color(255, 255, 204);
+    }
+
+    public Color getGoalKeeper() {
+        return new Color(85, 159, 68        );
+    }
+
+    public EstiloUniforme getStyle() {
+        return EstiloUniforme.FRANJA_VERTICAL;
+    }
+
+    public Color getShirtColor2() {
+        return new Color(250, 225, 157);
+    }
+
+    public Color getShortsColor2() {
+        return new Color(2, 208, 252);
+    }
+
+    public Color getShirtLineColor2() {
+        return new Color(228, 59, 1);
+    }
+
+    public Color getSocksColor2() {
+        return new Color(171, 121, 22);
+    }
+
+    public Color getGoalKeeper2() {
+        return new Color(29, 140, 107        );
+    }
+
+    public EstiloUniforme getStyle2() {
+        return EstiloUniforme.SIN_ESTILO;
+    }
+
+    class JugadorImpl implements PlayerDetail {
+
+        String nombre;
+        int numero;
+        Color piel, pelo;
+        double velocidad, remate, presicion;
+        boolean portero;
+        Position Position;
+
+        public JugadorImpl(String nombre, int numero, Color piel, Color pelo,
+                double velocidad, double remate, double presicion, boolean portero) {
+            this.nombre=nombre;
+            this.numero=numero;
+            this.piel=piel;
+            this.pelo=pelo;
+            this.velocidad=velocidad;
+            this.remate=remate;
+            this.presicion=presicion;
+            this.portero=portero;
+        }
+
+        public String getPlayerName() {
+            return nombre;
+        }
+
+        public Color getSkinColor() {
+            return piel;
+        }
+
+        public Color getHairColor() {
+            return pelo;
+        }
+
+        public int getNumber() {
+            return numero;
+        }
+
+        public boolean isGoalKeeper() {
+            return portero;
+        }
+
+        public double getSpeed() {
+            return velocidad;
+        }
+
+        public double getPower() {
+            return remate;
+        }
+
+        public double getPrecision() {
+            return presicion;
+        }
+
+    }
+
+    public PlayerDetail[] getPlayers() {
+        return new PlayerDetail[]{
+            new JugadorImpl("Mooroh", 1, new Color(51,0,51), new Color(0,0,0),1.0d,1.0d,1.0d, true),
+            new JugadorImpl("Antonio", 2, new Color(255,200,150), new Color(50,0,0),1.0d,1.0d,0.7d, false),
+            new JugadorImpl("Miguel", 3, new Color(255,200,150), new Color(50,0,0),1.0d,1.0d,0.66d, false),
+            new JugadorImpl("Jose", 4, new Color(255,200,150), new Color(50,0,0),0.77d,0.62d,0.61d, false),
+            new JugadorImpl("Jesus", 5, new Color(255,200,150), new Color(50,0,0),0.82d,0.64d,0.63d, false),
+            new JugadorImpl("Javier", 6, new Color(255,200,150), new Color(50,0,0),1.0d,0.49d,0.7d, false),
+            new JugadorImpl("Luis", 7, new Color(255,200,150), new Color(50,0,0),1.0d,0.51d,0.68d, false),
+            new JugadorImpl("Alberto", 8, new Color(255,200,150), new Color(50,0,0),1.0d,0.54d,0.69d, false),
+            new JugadorImpl("Carlos", 9, new Color(255,200,150), new Color(50,0,0),1.0d,0.76d,0.68d, false),
+            new JugadorImpl("Quique", 10, new Color(255,200,150), new Color(50,0,0),1.0d,1.0d,1.0d, false),
+            new JugadorImpl("Jorge", 11, new Color(255,200,150), new Color(50,0,0),1.0d,1.0d,1.0d, false)
+        };
+    }
+}
+
